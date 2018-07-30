bc753be703785f11667f324d93e8474127d7daf0
unknown <wroehrman@yahoo.com>
2018-07-29 21:30:28 -0500

    update to node packages and bamazon files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-// var inquirer = require("inquirer");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -28,9 +28,62 @@ var displayProducts = function () {
         for (var i = 0; i < res.length; i++) {
             console.log("Item ID: " + res[i].itemID + " || Product: " + res[i].productname + " || Department: " + res[i].departmentname + " || Price: " + res[i].price + " || Stock: " + res[i].stockquantity);
         }
-        connection.end();
+        // connection.end();
+        start();
 
     })
 
 };
 
+function start() {
+    inquirer
+        .prompt([
+            {
+
+
+                name: "Product",
+                type: "input",
+                message: "What is the ID of the product you would like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) == false) {
+                        return true;
+                    }
+                    return false;
+
+
+                }
+            },
+            {
+                name: "Quantity",
+                type: "input",
+                message: "How many units would you like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) == false) {
+                        return true;
+                    }
+                    return false;
+
+                }
+
+            }
+        ]).then(function (answer) {
+            var query = "SELECT * FROM Products WHERE itemID =" + answer.Quantity;
+
+            connection.query(query, function (err, res) {
+                if (answer.Quantity <= res) {
+                    for (var i = 0; i < res.length; i++) {
+                        console.log("There are" + res[i].stockquantity + "total left of" + res[i].productname + ".");
+                        console.log("Thank for for your order." + res[i].stockquantity + "pieces of" + res[i].productname + "is on the way!");
+                    }
+
+                }
+                else {
+                    console.log("Not enough items in stock!");
+                }
+                displayProducts();
+            })
+        })
+
+
+
+}
